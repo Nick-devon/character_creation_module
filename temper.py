f416b89f4f216dd3dd296259757553882de12e15
Никита Папенин <nikitapapenin@yandex.ru>
2023-04-27 17:58:16 +0300

    OOP design code

--- temper.py
+++ temper.py
@@ -1,22 +1,48 @@
-from math import sqrt
+# Импортируйте datetime. 
+import datetime as dt
+# Импортируйте time.
+import time
 
-message = ('Добро пожаловать в самую лучшую программу для вычисления квадратного корня из заданного числа')
-print(message)
+class Quest:
+    
+    def __init__(self, name, description, goal):
+        self.name = name
+        self.description = description
+        self.goal = goal
+        self.start_time = None
+        self.end_time = None
+        # Допишите два свойства класса.
+
+        
+    def accept_quest(self):
+        if self.end_time != None:
+            return 'С этим испытанием вы уже справились'
+        self.start_time = dt.datetime.now()
+        return f'Начало "{self.name}" положено'
+  
+
+    def pass_quest(self):
+        self.end_time = dt.datetime.now()
+        if self.start_time == None:
+            return 'Нельзя завершить то, что не имеет начала!'
+        completion_time = self.end_time - self.start_time
+        return (f'Квест "{self.name}" окончен. Время выполнения квеста: '
+                f'{completion_time}')
+        
+    # Напишите методы приема и сдачи квеста.
 
 
-def calculatesquareroot(number):
-    """Вычисляет квадратный корень"""
-    return sqrt(number)
+quest_name = 'Сбор пиксельники'
+quest_goal = 'Соберите 12 ягод пиксельники.'
+quest_description = '''
+В древнем лесу Кодоборье растёт ягода "пиксельника".
+Она нужна для приготовления целебных снадобий.
+Соберите 12 ягод пиксельники.'''
 
+new_quest = Quest(quest_name, quest_description, quest_goal) 
 
-def calc(your_number):
-    if your_number <= 0:
-        return
-
-    print(
-        f'Мы вычислили квадратный корень из введённого вами числа.'
-        f'Это будет: {calculatesquareroot(your_number)}')
-
-
-print(message)
-calc(25.5)
+print(new_quest.pass_quest())
+print(new_quest.accept_quest())
+time.sleep(3)
+print(new_quest.pass_quest())
+print(new_quest.accept_quest())
